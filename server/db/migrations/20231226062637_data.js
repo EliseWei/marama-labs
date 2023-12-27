641215fdeba4509c698d7b2e22d3e2ade6ebf9c5
Elise Wei <elise.wei@gmail.com>
2023-12-27 20:20:06 +1300

    Setup DB and prepare to use

---
+++ server/db/migrations/20231226062637_data.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.createTable('data', (table) => {
+    table.increments('id').primary()
+    table.string('name')
+    table.integer('parent_id').nullable().references('data.id')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.dropTable('data')
+}
