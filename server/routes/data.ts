687ad43fe0b3b9ede1f2c9dd7a8a00984bf3c485
Elise Wei <elise.wei@gmail.com>
2023-12-27 20:20:23 +1300

    Implement DB query from endpoint

--- server/routes/data.ts
+++ server/routes/data.ts
@@ -1,11 +1,30 @@
 import express from 'express'
-import { backendData } from '../data'
+import { getAllData } from '../db/data'
+import { DataItem, DataItemDb } from '../../models/data'
 
 const router = express.Router()
 
 // GET /api/v1/data
 router.get('/', async (req, res) => {
-  res.json({ data: backendData })
+  const flatItems = (await getAllData()) as DataItemDb[]
+  const topLevelItems = flatItems.filter((item) => !item.parentId)
+  const descendantItems = flatItems.filter((item) => item.parentId)
+
+  function addChildren(parent: DataItem, descArray: DataItemDb[]) {
+    const myChildren = descArray.filter((item) => item.parentId === parent.id)
+    if (myChildren.length) {
+      parent.children = myChildren.map((child) => {
+        delete child.parentId
+        return addChildren(child, descArray)
+      })
+    }
+    return parent
+  }
+  const nestedItems = topLevelItems.map((item) => {
+    delete item.parentId
+    return addChildren(item, descendantItems)
+  })
+  res.json({ data: nestedItems })
 })
 
 export default router
